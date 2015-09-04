b7d106bfcb62bddf4e8e70ef06bc2b9328dc11ce
unknown <wjiang@137IT3FL49927W7.dsnyad.nycnet>
2015-09-04 14:47:52 -0400

    save crud

--- src/main/web/js/app.js
+++ src/main/web/js/app.js
@@ -1,1 +1,27 @@
-angular.module('app', []);
+/**
+ * Created by Sandeep on 01/06/14.
+ */
+
+angular.module('movieApp',['ui.router','ngResource','movieApp.controllers','movieApp.services']);
+
+angular.module('movieApp').config(function($stateProvider,$httpProvider){
+    $stateProvider.state('events',{
+        url:'/events',
+        templateUrl:'partials/movies.html',
+        controller:'MovieListController'
+    }).state('viewMovie',{
+       url:'/events/:id/view',
+       templateUrl:'partials/movie-view.html',
+       controller:'MovieViewController'
+    }).state('newMovie',{
+        url:'/events/new',
+        templateUrl:'partials/movie-add.html',
+        controller:'MovieCreateController'
+    }).state('editMovie',{
+        url:'/events/:id/edit',
+        templateUrl:'partials/movie-edit.html',
+        controller:'MovieEditController'
+    });
+}).run(function($state){
+   $state.go('events');
+});

b0c1768247d1903c6ce6b6e0e5da22ede5141cab
Eddy Zhu <ezhu@dsny.nyc.gov>
2015-09-04 10:30:30 -0400

    first commit

---
+++ src/main/java/com/dsny/hack/DsnyhackApplication.java
@@ -0,0 +1,12 @@
+package com.dsny.hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsnyhackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsnyhackApplication.class, args);
+    }
+}
